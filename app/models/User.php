a45e36a20ee897818e7377219a746fbf58768ce0
stygiansabyss <stygian.warlock.v2@gmail.com>
2014-05-05 04:43:06 -0500

    Testing new user set up for models.

---
+++ app/models/User.php
@@ -0,0 +1,9 @@
+<?php
+
+use Syntax\Core\User as CoreUser;
+use Syntax\Core\Traits\ForumableUserTrait;
+
+class User extends CoreUser {
+    // If you want to use forum, uncomment the line below
+    // use ForumableUserTrait;
+}
